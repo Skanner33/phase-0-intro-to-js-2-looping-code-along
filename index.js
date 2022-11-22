9fb8c24bdbeb7a4ed181a1a4231c2e1a92c3207a
Skanner33 <mathewmageto@gmail.com>
2022-11-22 15:26:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(eArray,Revent){
+    let nArr = [];
+
+    for(let c = 0; c < eArray.length; c++){
+        nArr[c] = `Thank you, ${eArray[c]}, for the wonderful ${Revent} gift!`;
+    }
+}
+
+function countDown(rNum){
+    while(rNum >= 0){
+        console.log(rNum--);
+    }
+    return rNum;
+}
